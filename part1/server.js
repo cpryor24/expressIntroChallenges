5cc38901cf8e241061c1fd2fa24d1a6f6df984cc
Chris Pryor <chrispryor@live.com>
2018-09-14 08:56:56 -0700

    completed challenges

--- part1/server.js
+++ part1/server.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+let bodyParser = require('body-parser');
+let fs = require('fs');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+app.use(bodyParser.json());
+
+app.get('/hello', function(req, res) {
+  res.send("Hello");
+});
+
+app.post('/create/:name', (req, res) =>{
+  let user = {
+    id: 2,
+    name: req.params.name
+  }
+  res.send(user);
+});
+
+app.get('/', (req, res) => {
+  let index = fs.readFileSync('part1/index.html', 'utf8');
+  res.send(index);
+});
+
+app.get('/verify/:age', (req, res) => {
+  if(req.params.age > 13){
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(403);
+  }
 });
 
 app.use(function(req, res) {
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,42 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
+let bodyParser = require('body-parser');
+let fs = require('fs');
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+
+
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  let data = fs.readFileSync('part2/storage.json', 'utf8')
+    res.send(data);
 });
+
+app.post('/create/:name/:age', (req, res) => {
+  let storage = fs.readFileSync(__dirname + '/storage.json', 'utf8');
+  let data = JSON.parse(storage);
+  let user = {
+    name: req.params.name,
+    age: req.params.age
+  }
+
+  data.push(user)
+  fs.writeFileSync(__dirname + '/storage.json', JSON.stringify(data));
+  res.send(data);
+})
+
+app.get('/:name', (req, res) => {
+  let firstObj = JSON.parse(fs.readFileSync('part2/storage.json', 'utf8'));
+  let user = firstObj.find(el => {
+    return el.name === req.params.name
+  })
+  if(user){
+    res.send(user)
+  } else {
+    res.sendStatus(400);
+  }
+})
 
 app.use(function(req, res) {
   res.sendStatus(404);
